98c4a9183080c66c9b9cc43df71613db732c4cc0
Snehangsu De <63997773+snehangsude@users.noreply.github.com>
2022-03-22 20:07:02 +0530

    Change database
    Sqllite > Postgres

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from graph import Plot
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 Bootstrap(app)
