89d4ec3b956b79aed8ccac3fa4a4f17fce117109
Snehangsu De <63997773+snehangsude@users.noreply.github.com>
2022-03-22 20:33:07 +0530

    update uri

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from graph import Plot
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 Bootstrap(app)
